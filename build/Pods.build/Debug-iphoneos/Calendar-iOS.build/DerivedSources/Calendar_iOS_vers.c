6fed4e3c175fdf01f7796e0bf3ccdb15bf18f7e1
udaykiran.Ailapaka <udaykiran.auk@gmail.com>
2016-07-05 23:15:13 +0530

    First Eat - Subscription commit

---
+++ build/Pods.build/Debug-iphoneos/Calendar-iOS.build/DerivedSources/Calendar_iOS_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char Calendar_iOSVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Calendar_iOS  PROJECT:Pods-1" "\n"; const double Calendar_iOSVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Pods.build/Debug-iphoneos/Pods-MyBreakFast.build/DerivedSources/Pods_MyBreakFast_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char Pods_MyBreakFastVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MyBreakFast  PROJECT:Pods-1" "\n"; const double Pods_MyBreakFastVersionNumber __attribute__ ((used)) = (double)1.;
